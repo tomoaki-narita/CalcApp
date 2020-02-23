c5484bd901df80da9e13e49233ad9ed43e1361e4
output <output.org@gmail.com>
2020-02-23 21:42:42 +0900

    2020/02/23/21:42

--- CalcApp/Model/Car.swift
+++ CalcApp/Model/Car.swift
@@ -8,21 +8,27 @@
 
 import Foundation
 
-//carクラス(設計図)を作る
+//carクラス(設計図)を定義
 class Car{
     
-    //プロパティーを作る
+    //プロパティー(変数)を定義
+    //この場合、0(数字)なので型推論でInt型となる
     var frontWheel = 0
     var rearWheel = 0
     
-    //イニシャライズ(初期化)をする。設計図に必ず書かなければならない
+    //プロパティーをイニシャライザ(初期化)をする。実体化できるようになる
+    //クラス(設計図)に必ず書かなければならない
+    //initメソッドでイニシャライザした値はクラスの外から、
+    //クラスの オブジェクト.プロパティ名 を指定して参照することができるようになる
     init(){
         
         //frontWheel,rearWheelにいくつ入れるか記述
+        //変数名 = 初期値(初期化)
         frontWheel = 0
         rearWheel = 0
     }
     
+    //メソッド1
     //function(関数、機能)を記述。メソッドとは違う?
     //データを持つオブジェクトに対する操作をメソッドと呼ぶ
     func drive(){
@@ -34,9 +40,21 @@ class Car{
         
     }
     
+    //メソッド2
+    //String型は文字列、Int型は数字
     func move(toBack:String){
         
         print(toBack)
+    }
+    
+    //メソッド3
+    //frontWheelとrearWheelを足した値を出すメソッド
+    //()の中のIntは、インプットの型(Int型で書いてくださいと言う指定)
+    //->Intは、アウトプットの型
+    func plusAndMinus(num1:Int,num2:Int)->Int{
+        
+        //Int型の値をreturnする
+        return num1 + num2
     }
     
 }
--- CalcApp/ViewController.swift
+++ CalcApp/ViewController.swift
@@ -10,23 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //carModel(変数)でCar(class,設計図)が使えるようになった
+    //carModel(変数名)でCar,swiftのCarクラス(設計図)が使えるようになる
+
     var carModel = Car()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //carModelのfrontWheelとrearWheelに10を代入
+        //Car.swiftでイニシャライズ(初期化)しているので
+        //10がデバッグされる
         carModel.frontWheel = 10
         carModel.rearWheel = 10
         
     }
-
+    //結果ボタンが押された時の処理
     @IBAction func doAction(_ sender: Any) {
         
-        //Car.swiftのcarModelクラスのdriveメソッドを呼ぶ
+        //Car.swiftのdriveメソッドを呼ぶ
         carModel.drive()
         
+        //Car.swiftのmoveメソッドを呼ぶ
         carModel.move(toBack: "後ろに行くよ！！")
+        
+        //定数totalにCar.swiftのplusAndMinusメソッドを代入
+        let total = carModel.plusAndMinus(num1: carModel.frontWheel, num2: carModel.rearWheel)
+        
+        //Car.swiftのplusAndMinusメソッドでnum1+num2
+        //としているのでnum1とnum2を足した値がtotalに出力
+        print("タイヤの合計数  \(total)")
+        
         
     }
     
